645c6651417ea1dd0aafff2e79a65259753759dc
Didictateur <105631786+Didictateur@users.noreply.github.com>
2023-07-04 16:53:45 +0200

    Update help.py

--- code/help.py
+++ code/help.py
@@ -1,1 +1,26 @@
+from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import * 
+from PyQt5.QtGui import *
 
+class HelpWindow(QMainWindow):
+   def __init__(self):
+      super().__init__(self)
+      self.setWindowTitle("Help")
+      self.initUI()
+
+   def initUI(self):
+      self.tab = QTabWidget(self)
+
+      self.tabTile = QTabWidget()
+      self.tabDraw = QTabWidget()
+      self.tabMap = QTabWidget()
+
+      self.tab.addTab(self.tabTile, "Tile")
+      self.tab.addTab(self.tabDraw, "Draw")
+      self.tab.addTab(self.tabMap, "Map")
+
+      self.setupTile()
+      self.setupDraw()
+      self.setupMap()
+
+      self.setCentralWidget(self.tab)
