919ba75eab63113741ce8377ed00e2f786c45365
Adrien <decosse.adrien@gmail.com>
2023-07-05 14:12:51 +0200

    update help

--- code/help.py
+++ code/help.py
@@ -11,12 +11,12 @@ root_path = current_path.parent.parent
 class HelpWindow(QMainWindow):
    def __init__(self):
       super().__init__()
-      self.setWindowTitle("Help")
+      self.setWindowTitle("Tutorial")
       self.initUI()
 
    def initUI(self):
       self.mainWindow = QWidget(self)
-      self.mainWindow.setMinimumSize(500, 800)
+      self.mainWindow.setMinimumSize(580, 800)
       
       layout = QVBoxLayout(self.mainWindow)
       self.tab = QTabWidget(self)
@@ -26,18 +26,21 @@ class HelpWindow(QMainWindow):
       self.tabTile = QWidget()
       self.tabDraw = QWidget()
       self.tabMap = QWidget()
+      self.tabShort = QWidget()
 
       self.tab.addTab(self.tabHierarchy, "How a map is constitute")
       self.tab.addTab(self.tabMain, "Main window")
       self.tab.addTab(self.tabTile, "Tile")
       self.tab.addTab(self.tabDraw, "Draw")
       self.tab.addTab(self.tabMap, "Map")
+      self.tab.addTab(self.tabShort, "Shortcuts")
 
       self.setupHierarchy()
       self.setupMainWindow()
       self.setupTile()
-      # self.setupDraw()
+      self.setupDraw()
       # self.setupMap()
+      self.setupShort()
 
       layout.addWidget(self.tab)
       self.setCentralWidget(self.mainWindow)
@@ -91,9 +94,95 @@ class HelpWindow(QMainWindow):
    
    def setupTile(self):
       layout = QVBoxLayout(self.tabTile)
+      scrollArea = QScrollArea()
       
-      label = QLabel("yo")
-      layout.addWidget(label)
+      #Set scroll area
+      contentWidget = QWidget()
+      contentLayout = QVBoxLayout(contentWidget)
+      scrollArea.setWidget(contentWidget)
+      scrollArea.setWidgetResizable(True)
+      
+      #Body of the tab      
+      img = QLabel()
+      #pixmap = QPixmap(str(current_path)+"/img/helper/Hierarchy/Mappor.png")
+      pixmap = QPixmap("./img/helper/TileMod.png")
+      ratio = pixmap.size().height()/pixmap.size().width()
+      newsize = 500
+      pixmap = pixmap.scaled(newsize, int(newsize*ratio), Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothTransformation)
+      img.setPixmap(pixmap)
+      contentLayout.addWidget(img)
+      
+      #Finish scroll area
+      layout.addWidget(scrollArea)
+      
+   def setupDraw(self):
+      layout = QVBoxLayout(self.tabDraw)
+      scrollArea = QScrollArea()
+      
+      #Set scroll area
+      contentWidget = QWidget()
+      contentLayout = QVBoxLayout(contentWidget)
+      scrollArea.setWidget(contentWidget)
+      scrollArea.setWidgetResizable(True)
+      
+      #Body of the tab      
+      img = QLabel()
+      #pixmap = QPixmap(str(current_path)+"/img/helper/Hierarchy/Mappor.png")
+      pixmap = QPixmap("./img/helper/DrawMod.png")
+      ratio = pixmap.size().height()/pixmap.size().width()
+      newsize = 500
+      pixmap = pixmap.scaled(newsize, int(newsize*ratio), Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothTransformation)
+      img.setPixmap(pixmap)
+      contentLayout.addWidget(img)
+      
+      #Finish scroll area
+      layout.addWidget(scrollArea)
+   
+   def setupHierarchy(self):
+      layout = QVBoxLayout(self.tabHierarchy)
+      scrollArea = QScrollArea()
+      
+      #Set scroll area
+      contentWidget = QWidget()
+      contentLayout = QVBoxLayout(contentWidget)
+      scrollArea.setWidget(contentWidget)
+      scrollArea.setWidgetResizable(True)
+      
+      #Body of the tab      
+      img = QLabel()
+      #pixmap = QPixmap(str(current_path)+"/img/helper/Hierarchy/Mappor.png")
+      pixmap = QPixmap("./img/helper/Mappor.png")
+      ratio = pixmap.size().height()/pixmap.size().width()
+      newsize = 500
+      pixmap = pixmap.scaled(newsize, int(newsize*ratio), Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothTransformation)
+      img.setPixmap(pixmap)
+      contentLayout.addWidget(img)
+      
+      #Finish scroll area
+      layout.addWidget(scrollArea)
+      
+   def setupShort(self):
+      layout = QVBoxLayout(self.tabShort)
+      scrollArea = QScrollArea()
+      
+      #Set scroll area
+      contentWidget = QWidget()
+      contentLayout = QVBoxLayout(contentWidget)
+      scrollArea.setWidget(contentWidget)
+      scrollArea.setWidgetResizable(True)
+      
+      #Body of the tab      
+      img = QLabel()
+      #pixmap = QPixmap(str(current_path)+"/img/helper/Hierarchy/Mappor.png")
+      pixmap = QPixmap("./img/helper/Shortcuts.png")
+      ratio = pixmap.size().height()/pixmap.size().width()
+      newsize = 500
+      pixmap = pixmap.scaled(newsize, int(newsize*ratio), Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothTransformation)
+      img.setPixmap(pixmap)
+      contentLayout.addWidget(img)
+      
+      #Finish scroll area
+      layout.addWidget(scrollArea)
       
    def resizeEvent(self, event):
       super().resizeEvent(event)
