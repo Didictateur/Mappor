f9aa889c3777948c949894d5e22ed1ac076a2a63
Adrien <decosse.adrien@gmail.com>
2023-07-06 15:16:09 +0200

    Tree class

--- code/tree.py
+++ code/tree.py
@@ -12,35 +12,118 @@ class Tree:
     self.expanded = False
 
     self.child = []
+    
+    if self.root_path != None:
+      Lpath = root_path.split('/')
+      self.name = Lpath[-1]
+      self.absolutePath = self.root_path
+      self.folder = os.path.isdir(self.absolutePath)
+      for file in os.listdir(self.absolutePath):
+        self.child.append(Tree.initChild(self, file))
+  
+  def __eq__(self, tree: object) -> bool:
+    return self.absolutePath == tree.absolutePath
 
+  def __neq__(self, tree: object) -> bool:
+    return not self==tree
+  
+  def __le__(self, tree: object) -> bool:
+    return self.absolutePath <= tree.absolutePath
+  
+  def __ge__(self, tree: object) -> bool:
+    return self.absolutePath >= tree.absolutePath
+  
+  def __lt__(self, tree: object) -> bool:
+    return self.absolutePath < tree.absolutePath
+  
+  def __gt__(self, tree: object) -> bool:
+    return self.absolutePath > tree.absolutePath
+  
+  def list(self) -> list:
+    L = [self]
+    for child in self.child:
+      L += child.list()
+    return L
+  
   def copyInfo(self, tree: object) -> None:
-    """Only copy the information of the topest"""
+    """Only copy the information of the topest. The second object is calqued to the first one"""
     self.root_path = tree.root_path
     self.pathFromRoot = tree.pathFromRoot
     self.name = tree.name
     self.folder = tree.folder
     self.expanded = tree.expanded
-
+    
   @staticmethod
-  def merge(t1: object, t2: object) -> object:
-    """If the is similarity, apply t1 to t2, then returs the new t2"""
+  def initChild(parentTree, fileName) -> object:
+    tree = Tree()
+    tree.root_path = parentTree.root_path
+    tree.pathFromRoot = parentTree.pathFromRoot + '/' +fileName
+    tree.absolutePath = tree.root_path+tree.pathFromRoot
+    tree.name = fileName
+    tree.folder = os.path.isdir(tree.absolutePath)
+    if tree.folder:
+      for file in os.listdir(tree.absolutePath):
+        tree.child.append(Tree.initChild(tree, file))
+    return tree
+    
+  @staticmethod
+  def fuse(t1: object, t2: object) -> None:
+    """If the is similarity, apply t1 to t2, then don't returns the new t2"""
+    if t1.absolutePath == t2.absolutePath:
+      t2.copyInfo(t1)
+      commonChild, t1child, t2child = intersection(t1.child, t2.child)
+      t1.child = sorted(t1.child)
+      t2.child = sorted(t2.child)
+      i1 = 0
+      i2 = 0
+      while i1 < len(commonChild):
+        if t1.child[i1] in commonChild:
+          if t2.child[i2] not in commonChild:
+              i2 += 1
+          else:
+            Tree.fuse(t1.child[i1], t2.child[i2])
+            i1 += 1
+            i2 += 1
+        else:
+          i1 += 1
+    
+  @staticmethod
+  def merge(t1: object, t2: object) -> None:
+    """If the is similarity, apply t1 to t2, then don't returns the new t2, even in the middle"""
     if t1.absolutePath != t2.absolutePath:
-      return t2
-
-
+      if t1 in t2.list():
+        for child in t2.child:
+          if t1 in child.list():
+            Tree.merge(t1, child)
+      elif t2 in t1.list():
+        for child in t1.child:
+          if t2 in child.list():
+            Tree.merge(child, t2)
+    else:
+      Tree.fuse(t1, t2)
 
 def intersection(L1, L2):
   """returns :
     the intersection
     the surplus of L1
-    the surplus of L2"""
+    the surplus of L2
+  """
   Inter = []
   l1 = L1.copy()
-  l1p = []
+  lr1 = []
   while l1:
     x = l1.pop()
     if x in L2:
       Inter.append(x)
     else:
-      l1p.append(x)
-  l2p = []
+      lr1.append(x)
+  lr2 = [x for x in L2 if not x in Inter]
+  return Inter, lr1, lr2
+
+if __name__=="__main__":
+  tree = Tree("/home/decosse/Bureau/saves")
+  otherTree = Tree("/home/decosse/Bureau/saves/tile")
+  otherTree.expanded = True
+  input()
+  Tree.merge(otherTree, tree)
+  input()
