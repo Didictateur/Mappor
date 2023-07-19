cd84479ee4e11c11e096e13c7e2443fde0d3988b
Didictateur <105631786+Didictateur@users.noreply.github.com>
2023-07-19 11:47:17 +0200

    Update viewer.py

--- code/viewer.py
+++ code/viewer.py
@@ -944,7 +944,7 @@ class MainWindow(QMainWindow):
         tutoAction.triggered.connect(self.helpMenu)
         
         #info
-        infoAction = QAction("Version 1.0", self)
+        infoAction = QAction("Version 1.0.1", self)
 
         # Menu Bar
         file_menu = self.menu.addMenu("&File")
